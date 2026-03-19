14eb149f38f82891ed74edc6f7f488e7c1f3fc66
Isaac-ux-1 <abrahamisaac025@gmail.com>
2026-03-19 00:22:14 +0000

    commit 4

--- wp-content/themes/isaac-pizza-burgers/functions.php
+++ wp-content/themes/isaac-pizza-burgers/functions.php
@@ -9,11 +9,10 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array('parent-style')
-    );
-    wp_enqueue_script('custom_javascript0', get_stylesheet_directory_uri() . '/customElements.js');
-    wp_enqueue_script_module('custom_javascript1', "https://isaac-ux-1.github.io/ux308isaacux-Assignment1/fab.js");
-    wp_enqueue_script_module('custom_javascript2', "https://isaac-ux-1.github.io/ux308isaacux-Assignment1/chat.js");
+                            );
+                            wp_enqueue_script('custom_javascript0', get_stylesheet_directory_uri() . '/customElements.js');
+                            wp_enqueue_script_module('custom_javascript1', "https://isaac-ux-1.github.io/ux308isaacux-Assignment1/fab.js");
+                            wp_enqueue_script_module('custom_javascript2', "https://isaac-ux-1.github.io/ux308isaacux-Assignment1/chat.js");
 }
-//
-// Your code goes below
-//
+                                        //
+                                        // Your code goes below
